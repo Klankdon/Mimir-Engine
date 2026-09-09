8f4ec031eb4283fb4dd5feedba9c6fe0e330914e
Klankdon <122425827+Klankdon@users.noreply.github.com>
2026-09-09 18:17:39 -0500

    Add httpx import to app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv; load_dotenv()
 import os
 import logging
+import httpx
 from contextlib import asynccontextmanager
 import asyncpg
 from fastapi import FastAPI, Request, HTTPException, status
